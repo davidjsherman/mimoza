4b148639e07ffe4d0387eea4595268183ef2ccfd
Anna Zhukova <zhutchok@gmail.com>
2013-11-12 19:28:53 +0000

    Bundled ubiquitous edges

--- people/Zhukova_Anna/model_maps/retile.sh
+++ people/Zhukova_Anna/model_maps/retile.sh
@@ -1,5 +1,3 @@
 rm -rf peroxisome
-gdal2tiles.py -p raster -z 0-4 -w none peroxisome.jpg peroxisome
-rm -rf peroxisome/3
-rm -rf peroxisome/4
+gdal2tiles.py -p raster -z 0-2 -w none peroxisome.jpg peroxisome
 gdal2tiles.py -p raster -z 3-4 -w none peroxisome_f.jpg peroxisome
