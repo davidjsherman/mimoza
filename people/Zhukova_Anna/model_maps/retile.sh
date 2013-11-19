7edf37243e19b0c92df0c57e1670834104ec00bf
Anna Zhukova <zhutchok@gmail.com>
2013-11-19 16:56:19 +0000

    Map borders fixed

--- people/Zhukova_Anna/model_maps/retile.sh
+++ people/Zhukova_Anna/model_maps/retile.sh
@@ -1,4 +1,4 @@
-rm -rf peroxisome
-gdal2tiles.py -p raster -z 0-3 -w none peroxisome.jpg peroxisome
-rm -rf peroxisome/3
-gdal2tiles.py -p raster -z 3-4 -w none peroxisome_f.jpg peroxisome
+rm -rf htdocs/peroxisome
+gdal2tiles.py -p raster -z 0-3 -w none peroxisome.jpg htdocs/peroxisome
+rm -rf htdocs/peroxisome/3
+gdal2tiles.py -p raster -z 3-4 -w none peroxisome_f.jpg htdocs/peroxisome
