937944bb27dfa0d5a875b2c6ee9edbd9846c3ba0
Anna Zhukova <zhutchok@gmail.com>
2015-04-23 17:26:10 +0000

    restructured files so that mimoza finds its ontologies

--- people/Zhukova_Anna/model_generalization/code/SBMLModelGeneralization/sbml_generalization/onto/onto_getter.py
+++ people/Zhukova_Anna/model_generalization/code/SBMLModelGeneralization/sbml_generalization/onto/onto_getter.py
@@ -1,3 +1,5 @@
+import logging
+
 __author__ = 'anna'
 
 import sbml_generalization
@@ -6,8 +8,8 @@ import os
 
 
 def get_chebi():
-    return "%s/onto/data/chebi.obo" % os.path.dirname(os.path.abspath(sbml_generalization.__file__))
+    return "%s/data/chebi.obo" % os.path.dirname(os.path.abspath(sbml_generalization.__file__))
 
 
 def get_go():
-    return "%s/onto/data/gene_ontology_ext.obo" % os.path.dirname(os.path.abspath(sbml_generalization.__file__))
+    return "%s/data/gene_ontology_ext.obo" % os.path.dirname(os.path.abspath(sbml_generalization.__file__))
